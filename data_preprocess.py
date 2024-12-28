19eff7f8f7810d99521705f97a1f41e5f2009884
atharvpatwardhan <atharvjpatwardhan@gmail.com>
2024-12-28 15:19:31 -0500

    Create One hot encoder module.

--- data_preprocess.py
+++ data_preprocess.py
@@ -5,37 +5,40 @@ from sklearn.preprocessing import OneHotEncoder
 
 fasta_source_file = 'data/test/HSP20.fasta'
 
-sequences = list(SeqIO.parse(fasta_source_file, 'fasta'))
-
-def clean_sequence(seq, valid_chars):
-    # Remove invalid characters
-    cleaned_seq = ''.join([c for c in seq if c in valid_chars])
-    return cleaned_seq
+sequences_data = list(SeqIO.parse(fasta_source_file, 'fasta'))
 
 #Valid characters in amino acids
 valid_chars = 'ACDEFGHIKLMNPQRSTVWY'
 
-cleaned_sequences = []
-for record in sequences:
-    cleaned_seq = clean_sequence(str(record.seq), valid_chars)
-    cleaned_sequences.append(cleaned_seq)
+def clean_sequence(seq, valid_chars):
+    #Remove invalid characters which are not amino acids.
+    cleaned_seq = ''.join([c for c in seq if c in valid_chars])
+    return cleaned_seq
 
 
-char_index_dict = {char: i for i, char in enumerate(valid_chars)}
-
-# Convert sequences to integer indices
-integer_encoded_sequences = [
-    [char_index_dict[char] for char in sequence] for sequence in cleaned_sequences
-]
-encoder = OneHotEncoder(categories=[list(range(len(valid_chars)))])
 
 
-one_hot_encoded_sequences = []
-for sequence in integer_encoded_sequences:
-    # Reshape to (n, 1) for sklearn compatibility
-    reshaped_sequence = np.array(sequence).reshape(-1, 1)
-    encoded_sequence = encoder.fit_transform(reshaped_sequence)
-    one_hot_encoded_sequences.append(encoded_sequence)
+def one_hot_encode_sequences(sequences):
+    cleaned_sequences = []
+    for record in sequences:
+        cleaned_seq = clean_sequence(str(record.seq), valid_chars)
+        cleaned_sequences.append(cleaned_seq)
 
-print("One-hot encoded shape:", one_hot_encoded_sequences[0].shape)
-print("One-hot encoded example:", one_hot_encoded_sequences[0])
+
+
+    #Convert sequences to integer indices to feed to one hot encoder
+    char_index_dict = {char: i for i, char in enumerate(valid_chars)}
+    integer_encoded_sequences = [
+        [char_index_dict[char] for char in sequence] for sequence in cleaned_sequences
+    ]
+    encoder = OneHotEncoder(categories=[list(range(len(valid_chars)))])
+
+
+    one_hot_encoded_sequences = []
+    for sequence in integer_encoded_sequences:
+        reshaped_sequence = np.array(sequence).reshape(-1, 1)
+        encoded_sequence = encoder.fit_transform(reshaped_sequence)
+        one_hot_encoded_sequences.append(encoded_sequence)
+
+    return one_hot_encoded_sequences
+
